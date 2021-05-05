65218c4d3171fa89b10607d59ea6720853a9e076
AlexanderPokhilenko <pokhilenko.alex@gmail.com>
2021-05-05 20:27:34 +0300

    Update CocomoCalculator

--- ProjectDifficultyCalculator/ProjectDifficultyCalculator.Logic/COCOMO/CocomoCalculator.cs
+++ ProjectDifficultyCalculator/ProjectDifficultyCalculator.Logic/COCOMO/CocomoCalculator.cs
@@ -1,7 +1,38 @@
-﻿namespace ProjectDifficultyCalculator.Logic.COCOMO
+﻿using System;
+using System.Linq;
+
+namespace ProjectDifficultyCalculator.Logic.COCOMO
 {
     public class CocomoCalculator
     {
-        //TODO
+        public readonly CocomoProperties Properties;
+
+        public CocomoCalculator(CocomoProperties properties)
+        {
+            Properties = properties;
+        }
+
+        public double CalculateScaleCoefficientE(params double[] scaleFactors)
+        {
+            return Properties.B + scaleFactors.Aggregate(0.01, (current, sf) => current * sf);
+        }
+
+        public double CalculateCorrectionFactorM(params double[] costDrivers)
+        {
+            return costDrivers.Aggregate((current, driver) => current * driver);
+        }
+
+        public double CalculateRequirementsChangeCoefficientK(double percentage)
+        {
+            return 1.0 + percentage / 100.0;
+        }
+
+        public double CalculateComplexityPM(uint size, double reqPercentage, double[] scaleFactors, double[] costDrivers)
+        {
+            var k = CalculateRequirementsChangeCoefficientK(reqPercentage);
+            var e = CalculateScaleCoefficientE(scaleFactors);
+            var m = CalculateCorrectionFactorM(costDrivers);
+            return Properties.A * k * Math.Pow(size, e) * m;
+        }
     }
 }
